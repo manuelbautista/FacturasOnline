c0b7025a3dbe4e850d90ebca432dfb66e64160a5
Manuel Bautista <b0hcoder@gmail.com>
2017-10-09 15:17:55 -0400

    Integrating Sentry.io, Toasty and some components

--- ClientApp/app/app.module.shared.ts
+++ ClientApp/app/app.module.shared.ts
@@ -1,3 +1,5 @@
+import { ErrorHandler } from '@angular/core';
+import { AppErrorHandler } from './app.error-handler';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -10,6 +12,13 @@ import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 import { ClientesComponent } from './components/clientes/clientes.component';
+import { FacturasComponent } from './components/facturas/facturas.component';
+import { ToastyModule } from 'ng2-toasty';
+import * as Raven from 'raven-js';
+
+Raven
+.config('https://de26a9be61604d0d93dfe0dc272f2d02@sentry.io/227746')
+.install();
 
 @NgModule({
     declarations: [
@@ -18,17 +27,23 @@ import { ClientesComponent } from './components/clientes/clientes.component';
         CounterComponent,
         FetchDataComponent,
         HomeComponent,
-        ClientesComponent
+        ClientesComponent,
+        FacturasComponent
+    ],
+    providers: [
+        { provide: ErrorHandler, useClass: AppErrorHandler},
     ],
     imports: [
         CommonModule,
         HttpModule,
+        ToastyModule.forRoot(),
         FormsModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'login', component: CounterComponent },
+            { path: 'clientes', component: ClientesComponent },
+            { path: 'facturas', component: FacturasComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
