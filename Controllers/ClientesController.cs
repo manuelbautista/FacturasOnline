0f7cd1171bfdf70f5540f12b314d0f1815de1425
Manuel Bautista <b0hcoder@gmail.com>
2017-10-08 21:39:14 -0400

    Setting Angular 4

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FacturasOnline.Controllers
+{
+    [Route("/api/clientes")]
+    public class ClientesController: Controller
+    {
+        
+        
+    }
+}
